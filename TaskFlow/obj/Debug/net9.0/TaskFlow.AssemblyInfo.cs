2179b118ae98734a74fb0554eb592a2f9579a500
Dreiken <santidreik@gmail.com>
2025-01-20 18:33:37 -0300

    Readme in bad position

--- TaskFlow/obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
+++ TaskFlow/obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b5636462afa35a7c97c9587690bb96b86e188e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
